43709714589bce76e4e5d8edc9fb2b77ccfa57f3
Michael Jendryke <michael.jendryke@geoinsight.ai>
2025-05-13 19:53:57 +0200

    added copyright notice to all *.rs files in src

--- src/adapters/dggal/dggal.rs
+++ src/adapters/dggal/dggal.rs
@@ -1,3 +1,11 @@
+// Copyright 2025 contributors to the GeoPlegmata project.
+//
+// Licenced under the Apache Licence, Version 2.0 <LICENCE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENCE-MIT or http://opensource.org/licenses/MIT>, at your
+// discretion. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use crate::ports::dggrs::DggrsPort;
 
 pub struct DggalImpl;
